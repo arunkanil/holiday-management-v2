76c96b0028099a9e749807126bc91971c1132bd7
Arun <arun@untanglestrategy.com>
2021-01-06 12:12:38 +0530

    Updates

---
+++ precache-manifest.58d81f63c061d4f3d79138a50838768f.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6600729811da9694fb6df06d9b3599c9",
+    "url": "/holiday-management-v2/index.html"
+  },
+  {
+    "revision": "3558be4cdcdac6d97c9d",
+    "url": "/holiday-management-v2/static/css/0.de1c6f3c.chunk.css"
+  },
+  {
+    "revision": "287142cc671d61ad8caf",
+    "url": "/holiday-management-v2/static/css/10.37243684.chunk.css"
+  },
+  {
+    "revision": "fea09ab40c0f5cd96acf",
+    "url": "/holiday-management-v2/static/css/11.37243684.chunk.css"
+  },
+  {
+    "revision": "e86307177f5cfe679fb4",
+    "url": "/holiday-management-v2/static/css/12.3fdfb815.chunk.css"
+  },
+  {
+    "revision": "8cbf348c06e3cc08a935",
+    "url": "/holiday-management-v2/static/css/13.12748e60.chunk.css"
+  },
+  {
+    "revision": "ad52b02bf0df1dc9bab0",
+    "url": "/holiday-management-v2/static/css/14.f0170cad.chunk.css"
+  },
+  {
+    "revision": "e3c8fa1e6dbf452f81d7",
+    "url": "/holiday-management-v2/static/css/15.4effb945.chunk.css"
+  },
+  {
+    "revision": "d7a8e9972d34900a7068",
+    "url": "/holiday-management-v2/static/css/2.ae59248a.chunk.css"
+  },
+  {
+    "revision": "740fd8c18cea3e98eb6e",
+    "url": "/holiday-management-v2/static/css/25.1faf741b.chunk.css"
+  },
+  {
+    "revision": "f8183d27643c167b6598",
+    "url": "/holiday-management-v2/static/css/26.819092da.chunk.css"
+  },
+  {
+    "revision": "c98de9a48fdd8de602cd",
+    "url": "/holiday-management-v2/static/css/27.efab397b.chunk.css"
+  },
+  {
+    "revision": "497c1d9f257a129a6f70",
+    "url": "/holiday-management-v2/static/css/28.efab397b.chunk.css"
+  },
+  {
+    "revision": "d13dce23877c05df9529",
+    "url": "/holiday-management-v2/static/css/29.908d57c2.chunk.css"
+  },
+  {
+    "revision": "c441008da045e49db9dd",
+    "url": "/holiday-management-v2/static/css/3.1892d553.chunk.css"
+  },
+  {
+    "revision": "505a10ab6076ce12f6df",
+    "url": "/holiday-management-v2/static/css/30.1892d553.chunk.css"
+  },
+  {
+    "revision": "5481779414d90f64a726",
+    "url": "/holiday-management-v2/static/css/31.debc43ae.chunk.css"
+  },
+  {
+    "revision": "fa13cd027557753f9a37",
+    "url": "/holiday-management-v2/static/css/32.4e22eb58.chunk.css"
+  },
+  {
+    "revision": "ee7dcf74486079f86763",
+    "url": "/holiday-management-v2/static/css/33.819092da.chunk.css"
+  },
+  {
+    "revision": "d2e92427a9db07bd30db",
+    "url": "/holiday-management-v2/static/css/5.21298d47.chunk.css"
+  },
+  {
+    "revision": "6ec3ab3079a73cdd5a2d",
+    "url": "/holiday-management-v2/static/css/9.489224e3.chunk.css"
+  },
+  {
+    "revision": "67dab837cc4e3f80cc7f",
+    "url": "/holiday-management-v2/static/css/main.1bd98a53.chunk.css"
+  },
+  {
+    "revision": "3558be4cdcdac6d97c9d",
+    "url": "/holiday-management-v2/static/js/0.3d8d768c.chunk.js"
+  },
+  {
+    "revision": "fdb4764707a46562c9b7",
+    "url": "/holiday-management-v2/static/js/1.2e34993d.chunk.js"
+  },
+  {
+    "revision": "287142cc671d61ad8caf",
+    "url": "/holiday-management-v2/static/js/10.1a573ba2.chunk.js"
+  },
+  {
+    "revision": "fea09ab40c0f5cd96acf",
+    "url": "/holiday-management-v2/static/js/11.c5e00c91.chunk.js"
+  },
+  {
+    "revision": "e86307177f5cfe679fb4",
+    "url": "/holiday-management-v2/static/js/12.3cc5b656.chunk.js"
+  },
+  {
+    "revision": "8cbf348c06e3cc08a935",
+    "url": "/holiday-management-v2/static/js/13.dfb9fcb8.chunk.js"
+  },
+  {
+    "revision": "ad52b02bf0df1dc9bab0",
+    "url": "/holiday-management-v2/static/js/14.3d0bc44c.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-management-v2/static/js/14.3d0bc44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c8fa1e6dbf452f81d7",
+    "url": "/holiday-management-v2/static/js/15.d65072d9.chunk.js"
+  },
+  {
+    "revision": "bba2c82729aeb95ee616",
+    "url": "/holiday-management-v2/static/js/16.f966f958.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/16.f966f958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b65f8a66b37fae9f26",
+    "url": "/holiday-management-v2/static/js/17.49f1e7d2.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/17.49f1e7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1347b24129a99e4fb69c",
+    "url": "/holiday-management-v2/static/js/18.ca89cd87.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/18.ca89cd87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16147afe7056b5d0e202",
+    "url": "/holiday-management-v2/static/js/19.86621edc.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/19.86621edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a8e9972d34900a7068",
+    "url": "/holiday-management-v2/static/js/2.702878c5.chunk.js"
+  },
+  {
+    "revision": "30dbbfa5f27a2973f4d6",
+    "url": "/holiday-management-v2/static/js/20.48ffdecc.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/20.48ffdecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105c2109d554dfbd241d",
+    "url": "/holiday-management-v2/static/js/21.a2b9e719.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/21.a2b9e719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5218be866dafa34079",
+    "url": "/holiday-management-v2/static/js/22.10d564b2.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/22.10d564b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db65d78a95c73b7c36d",
+    "url": "/holiday-management-v2/static/js/23.f6eb2764.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/23.f6eb2764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650ab790d63a7357c6a1",
+    "url": "/holiday-management-v2/static/js/24.bb38fec5.chunk.js"
+  },
+  {
+    "revision": "390eacea26ecab493caf24c949d5efdb",
+    "url": "/holiday-management-v2/static/js/24.bb38fec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740fd8c18cea3e98eb6e",
+    "url": "/holiday-management-v2/static/js/25.42d2b529.chunk.js"
+  },
+  {
+    "revision": "f8183d27643c167b6598",
+    "url": "/holiday-management-v2/static/js/26.781e0a21.chunk.js"
+  },
+  {
+    "revision": "c98de9a48fdd8de602cd",
+    "url": "/holiday-management-v2/static/js/27.9f710c4f.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-management-v2/static/js/27.9f710c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497c1d9f257a129a6f70",
+    "url": "/holiday-management-v2/static/js/28.3e5ec7bf.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-management-v2/static/js/28.3e5ec7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13dce23877c05df9529",
+    "url": "/holiday-management-v2/static/js/29.f9dc4f3e.chunk.js"
+  },
+  {
+    "revision": "c441008da045e49db9dd",
+    "url": "/holiday-management-v2/static/js/3.89f62f88.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-management-v2/static/js/3.89f62f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505a10ab6076ce12f6df",
+    "url": "/holiday-management-v2/static/js/30.0aa79354.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "/holiday-management-v2/static/js/30.0aa79354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5481779414d90f64a726",
+    "url": "/holiday-management-v2/static/js/31.7720b0ef.chunk.js"
+  },
+  {
+    "revision": "fa13cd027557753f9a37",
+    "url": "/holiday-management-v2/static/js/32.5850793e.chunk.js"
+  },
+  {
+    "revision": "ee7dcf74486079f86763",
+    "url": "/holiday-management-v2/static/js/33.46ed6b03.chunk.js"
+  },
+  {
+    "revision": "f94cfde14aad16fa8f87",
+    "url": "/holiday-management-v2/static/js/34.2eeede09.chunk.js"
+  },
+  {
+    "revision": "943d0e2ebbbd55b88874",
+    "url": "/holiday-management-v2/static/js/4.96a36d42.chunk.js"
+  },
+  {
+    "revision": "d2e92427a9db07bd30db",
+    "url": "/holiday-management-v2/static/js/5.6ee2940c.chunk.js"
+  },
+  {
+    "revision": "7e80c8d8499b462d565e",
+    "url": "/holiday-management-v2/static/js/6.938c7732.chunk.js"
+  },
+  {
+    "revision": "6ec3ab3079a73cdd5a2d",
+    "url": "/holiday-management-v2/static/js/9.5197d5b4.chunk.js"
+  },
+  {
+    "revision": "39479d911a726fb8a97eb8dbf747ab4f",
+    "url": "/holiday-management-v2/static/js/9.5197d5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67dab837cc4e3f80cc7f",
+    "url": "/holiday-management-v2/static/js/main.2d182b8c.chunk.js"
+  },
+  {
+    "revision": "3bae1e1db57c8b240140",
+    "url": "/holiday-management-v2/static/js/runtime-main.17945c38.js"
+  },
+  {
+    "revision": "10ebf9189344922639b131e345327395",
+    "url": "/holiday-management-v2/static/media/401.10ebf918.png"
+  },
+  {
+    "revision": "6197a0901ba6b21a860b2327e138eba6",
+    "url": "/holiday-management-v2/static/media/404.6197a090.png"
+  },
+  {
+    "revision": "9a1bd4534302f92ea3b997ad76f1ec1d",
+    "url": "/holiday-management-v2/static/media/500.9a1bd453.png"
+  },
+  {
+    "revision": "0facaae97183b8fede52099930aefd8d",
+    "url": "/holiday-management-v2/static/media/HelveticaNeueLight.0facaae9.ttf"
+  },
+  {
+    "revision": "348dee448f78d73d826c49b0c1e89f6e",
+    "url": "/holiday-management-v2/static/media/abp-logo-long.348dee44.png"
+  },
+  {
+    "revision": "fd7c100fdd5537febf41cf83a32cb11c",
+    "url": "/holiday-management-v2/static/media/approval.fd7c100f.svg"
+  },
+  {
+    "revision": "2999d7dbc9597d512bebbe85cc796275",
+    "url": "/holiday-management-v2/static/media/deleteIcon.2999d7db.svg"
+  },
+  {
+    "revision": "2ab221362ac37906a8c6d998093d509e",
+    "url": "/holiday-management-v2/static/media/edit.2ab22136.svg"
+  },
+  {
+    "revision": "a445cfdebdda60460be577a82b2c92aa",
+    "url": "/holiday-management-v2/static/media/empdata.a445cfde.xlsx"
+  },
+  {
+    "revision": "0502e76342919a809bd6c72973c8ca3a",
+    "url": "/holiday-management-v2/static/media/microsoft.0502e763.svg"
+  },
+  {
+    "revision": "eb40ecbc9af69d9fb133db5a7cb1799f",
+    "url": "/holiday-management-v2/static/media/user.eb40ecbc.svg"
+  },
+  {
+    "revision": "4c29894eb4a9fb1265d30ed8eb995fda",
+    "url": "/holiday-management-v2/static/media/userLayout-Backround.4c29894e.svg"
+  },
+  {
+    "revision": "eecc431ec418863b91287aece712b12c",
+    "url": "/holiday-management-v2/static/media/web.eecc431e.config"
+  }
+]);
